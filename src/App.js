ee5f27723440f9fa9817ab36b24047a6a474aae8
Bhoomika1534 <113657388+Bhoomika1534@users.noreply.github.com>
2022-11-26 16:19:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorSelect from './Drop';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorSelect />
+    </div>
+  );
+}
+
+export default App;
